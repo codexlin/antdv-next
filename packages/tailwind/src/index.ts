70e1ffc5f15954606761c8e64e2d0ac50d151c56
aibayanyu20 <aibayanyu@qq.com>
2026-01-31 15:29:24 +0800

    feat(tailwind): support tailwindcss

---
+++ packages/tailwind/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @antdv-next/tailwind
+ *
+ * Ant Design Vue Tailwind CSS Plugin
+ * 将 Ant Design Vue 的 CSS 变量系统集成到 Tailwind CSS 中
+ */
+
+export { createAntdPlugin, default } from './plugin'
+export type { AntdPluginOptions, ColorName } from './types'
